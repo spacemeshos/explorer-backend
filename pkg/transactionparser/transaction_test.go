4e996b4ddaef15a62e435aa194339ae378f7b629
Kacper Sawicki <kacper@ksawicki.pl>
2022-12-19 18:16:22 +0100

    Disable parser for multisig txs

--- pkg/transactionparser/transaction_test.go
+++ pkg/transactionparser/transaction_test.go
@@ -2,21 +2,15 @@ package transactionparser_test
 
 import (
 	"bytes"
-	"math/rand"
-	"testing"
-	"time"
-
-	"github.com/oasisprotocol/curve25519-voi/primitives/ed25519"
 	"github.com/spacemeshos/go-scale"
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/go-spacemesh/genvm/core"
 	"github.com/spacemeshos/go-spacemesh/genvm/sdk"
-	sdkMultisig "github.com/spacemeshos/go-spacemesh/genvm/sdk/multisig"
 	sdkWallet "github.com/spacemeshos/go-spacemesh/genvm/sdk/wallet"
-	"github.com/spacemeshos/go-spacemesh/genvm/templates/multisig"
 	"github.com/spacemeshos/go-spacemesh/genvm/templates/wallet"
 	"github.com/spacemeshos/go-spacemesh/signing"
 	"github.com/stretchr/testify/require"
+	"testing"
 
 	"github.com/spacemeshos/explorer-backend/pkg/transactionparser"
 )
@@ -111,136 +105,136 @@ func TestSpend(t *testing.T) {
 	}
 }
 
-func TestSpawnMultisig(t *testing.T) {
-	table := []struct {
-		name     string
-		gasPrice uint64
-		ref      uint8
-		opts     []sdk.Opt
-		nonce    types.Nonce
-	}{
-		{
-			name:     "default gas price",
-			gasPrice: 1,
-			ref:      3,
-			opts:     []sdk.Opt{},
-			nonce: types.Nonce{
-				Counter:  1,
-				Bitfield: uint8(1),
-			},
-		},
-		{
-			name:     "non default gasPrice",
-			gasPrice: 2,
-			ref:      5,
-			opts:     []sdk.Opt{sdk.WithGasPrice(2)},
-			nonce: types.Nonce{
-				Counter:  2,
-				Bitfield: uint8(1),
-			},
-		},
-	}
-	for _, tc := range table {
-		testCase := tc
-		t.Run(testCase.name, func(t *testing.T) {
-			t.Parallel()
-
-			var pubs []ed25519.PublicKey
-			pks := make([]ed25519.PrivateKey, 0, 3)
-			for i := 0; i < 3; i++ {
-				pub, pk, err := ed25519.GenerateKey(rand.New(rand.NewSource(time.Now().UnixNano())))
-				require.NoError(t, err)
-				pubs = append(pubs, pub)
-				pks = append(pks, pk)
-			}
-
-			var agg *sdkMultisig.Aggregator
-			for i := 0; i < len(pks); i++ {
-				part := sdkMultisig.SelfSpawn(uint8(i), pks[i], multisig.TemplateAddress3, pubs, testCase.nonce, testCase.opts...)
-				if agg == nil {
-					agg = part
-				} else {
-					agg.Add(*part.Part(uint8(i)))
-				}
-			}
-			rawTx := agg.Raw()
-
-			decodedTx, err := transactionparser.Parse(scale.NewDecoder(bytes.NewReader(rawTx)), rawTx, 0)
-			require.NoError(t, err)
-			require.Equal(t, testCase.gasPrice, decodedTx.GetGasPrice())
-			for i := 0; i < len(pks); i++ {
-				//	require.Equal(t, []byte(pubs[i]), decodedTx.MultisigSpawnArgs.PublicKeys[i].Bytes())
-			}
-		})
-	}
-}
-
-func TestSpendMultisig(t *testing.T) {
-	table := []struct {
-		name     string
-		to       types.Address
-		gasPrice uint64
-		amount   uint64
-		opts     []sdk.Opt
-		nonce    types.Nonce
-		ref      uint8
-	}{
-		{
-			name:     "default gas price",
-			amount:   123,
-			gasPrice: 1,
-			to:       types.GenerateAddress(generatePublicKey(t)),
-			opts:     []sdk.Opt{},
-			ref:      3,
-			nonce: types.Nonce{
-				Counter:  1,
-				Bitfield: uint8(1),
-			},
-		},
-		{
-			name:     "non default gasPrice",
-			amount:   723,
-			gasPrice: 2,
-			ref:      5,
-			to:       types.GenerateAddress(generatePublicKey(t)),
-			opts:     []sdk.Opt{sdk.WithGasPrice(2)},
-			nonce: types.Nonce{
-				Counter:  2,
-				Bitfield: uint8(4),
-			},
-		},
-	}
-	for _, tc := range table {
-		testCase := tc
-		t.Run(testCase.name, func(t *testing.T) {
-			t.Parallel()
-
-			pubs := make([][]byte, 0, 3)
-			pks := make([]ed25519.PrivateKey, 0, 3)
-			for i := 0; i < 3; i++ {
-				pub, pk, err := ed25519.GenerateKey(rand.New(rand.NewSource(time.Now().UnixNano())))
-				require.NoError(t, err)
-				pubs = append(pubs, pub)
-				pks = append(pks, pk)
-			}
-			principal := sdkMultisig.Address(multisig.TemplateAddress3, pubs...)
-			agg := sdkMultisig.Spend(0, pks[0], principal, testCase.to, testCase.amount, testCase.nonce, testCase.opts...)
-			for i := 1; i < len(pks); i++ {
-				part := sdkMultisig.Spend(uint8(i), pks[i], principal, testCase.to, testCase.amount, testCase.nonce, testCase.opts...)
-				agg.Add(*part.Part(uint8(i)))
-			}
-			rawTx := agg.Raw()
-
-			decodedTx, err := transactionparser.Parse(scale.NewDecoder(bytes.NewReader(rawTx)), rawTx, 1)
-			require.NoError(t, err)
-			require.Equal(t, testCase.gasPrice, decodedTx.GetGasPrice())
-			require.Equal(t, testCase.to.String(), decodedTx.GetReceiver().String())
-			require.Equal(t, testCase.amount, decodedTx.GetAmount())
-			require.Equal(t, testCase.nonce.Counter, decodedTx.GetCounter())
-			require.Equal(t, principal.String(), decodedTx.GetPrincipal().String())
-		})
-	}
-}
+//func TestSpawnMultisig(t *testing.T) {
+//	table := []struct {
+//		name     string
+//		gasPrice uint64
+//		ref      uint8
+//		opts     []sdk.Opt
+//		nonce    types.Nonce
+//	}{
+//		{
+//			name:     "default gas price",
+//			gasPrice: 1,
+//			ref:      3,
+//			opts:     []sdk.Opt{},
+//			nonce: types.Nonce{
+//				Counter:  1,
+//				Bitfield: uint8(1),
+//			},
+//		},
+//		{
+//			name:     "non default gasPrice",
+//			gasPrice: 2,
+//			ref:      5,
+//			opts:     []sdk.Opt{sdk.WithGasPrice(2)},
+//			nonce: types.Nonce{
+//				Counter:  2,
+//				Bitfield: uint8(1),
+//			},
+//		},
+//	}
+//	for _, tc := range table {
+//		testCase := tc
+//		t.Run(testCase.name, func(t *testing.T) {
+//			t.Parallel()
+//
+//			var pubs []ed25519.PublicKey
+//			pks := make([]ed25519.PrivateKey, 0, 3)
+//			for i := 0; i < 3; i++ {
+//				pub, pk, err := ed25519.GenerateKey(rand.New(rand.NewSource(time.Now().UnixNano())))
+//				require.NoError(t, err)
+//				pubs = append(pubs, pub)
+//				pks = append(pks, pk)
+//			}
+//
+//			var agg *sdkMultisig.Aggregator
+//			for i := 0; i < len(pks); i++ {
+//				part := sdkMultisig.SelfSpawn(uint8(i), pks[i], multisig.TemplateAddress3, pubs, testCase.nonce, testCase.opts...)
+//				if agg == nil {
+//					agg = part
+//				} else {
+//					agg.Add(*part.Part(uint8(i)))
+//				}
+//			}
+//			rawTx := agg.Raw()
+//
+//			decodedTx, err := transactionparser.Parse(scale.NewDecoder(bytes.NewReader(rawTx)), rawTx, 0)
+//			require.NoError(t, err)
+//			require.Equal(t, testCase.gasPrice, decodedTx.GetGasPrice())
+//			for i := 0; i < len(pks); i++ {
+//				//	require.Equal(t, []byte(pubs[i]), decodedTx.MultisigSpawnArgs.PublicKeys[i].Bytes())
+//			}
+//		})
+//	}
+//}
+//
+//func TestSpendMultisig(t *testing.T) {
+//	table := []struct {
+//		name     string
+//		to       types.Address
+//		gasPrice uint64
+//		amount   uint64
+//		opts     []sdk.Opt
+//		nonce    types.Nonce
+//		ref      uint8
+//	}{
+//		{
+//			name:     "default gas price",
+//			amount:   123,
+//			gasPrice: 1,
+//			to:       types.GenerateAddress(generatePublicKey(t)),
+//			opts:     []sdk.Opt{},
+//			ref:      3,
+//			nonce: types.Nonce{
+//				Counter:  1,
+//				Bitfield: uint8(1),
+//			},
+//		},
+//		{
+//			name:     "non default gasPrice",
+//			amount:   723,
+//			gasPrice: 2,
+//			ref:      5,
+//			to:       types.GenerateAddress(generatePublicKey(t)),
+//			opts:     []sdk.Opt{sdk.WithGasPrice(2)},
+//			nonce: types.Nonce{
+//				Counter:  2,
+//				Bitfield: uint8(4),
+//			},
+//		},
+//	}
+//	for _, tc := range table {
+//		testCase := tc
+//		t.Run(testCase.name, func(t *testing.T) {
+//			t.Parallel()
+//
+//			pubs := make([][]byte, 0, 3)
+//			pks := make([]ed25519.PrivateKey, 0, 3)
+//			for i := 0; i < 3; i++ {
+//				pub, pk, err := ed25519.GenerateKey(rand.New(rand.NewSource(time.Now().UnixNano())))
+//				require.NoError(t, err)
+//				pubs = append(pubs, pub)
+//				pks = append(pks, pk)
+//			}
+//			principal := sdkMultisig.Address(multisig.TemplateAddress3, pubs...)
+//			agg := sdkMultisig.Spend(0, pks[0], principal, testCase.to, testCase.amount, testCase.nonce, testCase.opts...)
+//			for i := 1; i < len(pks); i++ {
+//				part := sdkMultisig.Spend(uint8(i), pks[i], principal, testCase.to, testCase.amount, testCase.nonce, testCase.opts...)
+//				agg.Add(*part.Part(uint8(i)))
+//			}
+//			rawTx := agg.Raw()
+//
+//			decodedTx, err := transactionparser.Parse(scale.NewDecoder(bytes.NewReader(rawTx)), rawTx, 1)
+//			require.NoError(t, err)
+//			require.Equal(t, testCase.gasPrice, decodedTx.GetGasPrice())
+//			require.Equal(t, testCase.to.String(), decodedTx.GetReceiver().String())
+//			require.Equal(t, testCase.amount, decodedTx.GetAmount())
+//			require.Equal(t, testCase.nonce.Counter, decodedTx.GetCounter())
+//			require.Equal(t, principal.String(), decodedTx.GetPrincipal().String())
+//		})
+//	}
+//}
 
 func generatePublicKey(t *testing.T) []byte {
 	buff := signing.NewEdSigner().ToBuffer()
