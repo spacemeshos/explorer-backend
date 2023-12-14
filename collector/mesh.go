4dd0569258402651d9fb81e94480ba7e82697ef9
Kacper Sawicki <kacper@ksawicki.pl>
2023-12-14 12:35:17 +0100

    Sync not processed txs

--- collector/mesh.go
+++ collector/mesh.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/spacemeshos/explorer-backend/utils"
 	"github.com/spacemeshos/go-spacemesh/common/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"io"
 	"time"
 
@@ -173,6 +174,39 @@ func (c *Collector) syncLayer(lid types.LayerID) error {
 			Smesher:     &pb.SmesherId{Id: reward.SmesherID.Bytes()},
 		}
 		c.listener.OnReward(r)
+	}
+
+	return nil
+}
+
+func (c *Collector) syncNotProcessedTxs() error {
+	txs, err := c.listener.GetTransactions(context.TODO(), &bson.D{{Key: "state", Value: 0}})
+	if err != nil {
+		return err
+	}
+
+	for _, tx := range txs {
+		txId, err := utils.StringToBytes(tx.Id)
+		if err != nil {
+			return err
+		}
+
+		state, err := c.transactionsClient.TransactionsState(context.TODO(), &pb.TransactionsStateRequest{
+			TransactionId:       []*pb.TransactionId{{Id: txId}},
+			IncludeTransactions: false,
+		})
+		if err != nil {
+			return err
+		}
+
+		txState := state.TransactionsState[0]
+
+		if txState != nil {
+			err := c.listener.UpdateTransactionState(context.TODO(), tx.Id, int32(txState.State))
+			if err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
