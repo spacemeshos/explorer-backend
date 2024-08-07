a0bfbb7ea8bd427e1ab94c337c797f7038c901aa
Kacper Sawicki <kacper@ksawicki.pl>
2024-08-07 14:29:43 +0200

    Fix

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/spacemeshos/go-spacemesh/common/types"
 	"github.com/spacemeshos/go-spacemesh/log"
 	"github.com/spacemeshos/go-spacemesh/timesync"
-	"github.com/spacemeshos/post/config"
 	"github.com/urfave/cli/v2"
 	"go.uber.org/zap"
 	"net/http"
@@ -188,7 +187,7 @@ func main() {
 			NodeClock:     clock,
 			Testnet:       testnetBoolFlag,
 			LabelsPerUnit: labelsPerUnit,
-			BitsPerLabel:  config.BitsPerLabel,
+			BitsPerLabel:  128,
 		}
 
 		var wg sync.WaitGroup
