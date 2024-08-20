22950dc6a243688c6a89af568608661c881ba85e
Kacper Sawicki <kacper@ksawicki.pl>
2024-08-20 15:07:48 +0200

    Add redis cache store

--- api/cache/cache.go
+++ api/cache/cache.go
@@ -3,17 +3,32 @@ package cache
 import (
 	"github.com/eko/gocache/lib/v4/cache"
 	"github.com/eko/gocache/lib/v4/marshaler"
+	"github.com/eko/gocache/lib/v4/store"
 	gocacheStore "github.com/eko/gocache/store/go_cache/v4"
+	redis_store "github.com/eko/gocache/store/redis/v4"
 	gocache "github.com/patrickmn/go-cache"
+	"github.com/redis/go-redis/v9"
+	"github.com/spacemeshos/go-spacemesh/log"
 	"time"
 )
 
+var RedisAddress = ""
 var Expiration time.Duration = 0
 var ShortExpiration = 5 * time.Minute
 
 func New() *marshaler.Marshaler {
-	client := gocache.New(Expiration, 6*time.Hour)
-	s := gocacheStore.NewGoCache(client)
-	manager := cache.New[any](s)
+	var manager *cache.Cache[any]
+	if RedisAddress != "" {
+		log.Info("using redis cache")
+		redisStore := redis_store.NewRedis(redis.NewClient(&redis.Options{
+			Addr: RedisAddress,
+		}), store.WithExpiration(Expiration))
+		manager = cache.New[any](redisStore)
+	} else {
+		log.Info("using memory cahe")
+		client := gocache.New(Expiration, 6*time.Hour)
+		s := gocacheStore.NewGoCache(client)
+		manager = cache.New[any](s)
+	}
 	return marshaler.New(manager)
 }
